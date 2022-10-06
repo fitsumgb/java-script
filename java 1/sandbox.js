e3492d20cec7fcecebb7c57c66bf334cdff81417
Fitsum Bocru <fitsumbocru@Fitsums-MacBook-Pro.local>
2022-10-05 22:00:53 -0700

    creat new project, apply string concatenation, string character, string length, string method

---
+++ java 1/sandbox.js
@@ -0,0 +1,27 @@
+let age=60;
+console.log(age);
+
+let year = 2019;
+console.log(year);
+let firstName = 'Evan';
+let lastName = 'Fitsum';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(fullName[2]);
+console.log(fullName[5]);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result);
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+console.log('hello world!')
+let email = 'fitsumgb@yahoo.com';
+console.log(email);
+
+console.log(fullName.toUpperCase());
